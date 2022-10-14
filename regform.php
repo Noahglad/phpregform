ee40b7bf1567ee3529f168f6d853704074d6103c
Noah Ayanbode <44483000+Noahglad@users.noreply.github.com>
2022-10-14 14:44:18 +0100

    Add files via upload

---
+++ regform.php
@@ -0,0 +1,27 @@
+<html>
+
+<head>
+      <title>Registration Form</title>
+      <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+</head>
+
+<body>
+
+      <h2>Registration Form</h2>
+
+      <form action="regform.php" method="POST"> Name:
+
+            <input type="text" name="name"> <br> Email:
+
+            <input type="email" name="email"> <br>Date of Birth:
+            <input type="date" name="dob"> <br> Gender:
+
+            <input type="text" name="gender"> <br>Country:
+            <input type="text" name="country"><br>
+
+            <input type="submit" value="Submit">
+
+      </form>
+</body>
+
+</html>
